3dca88b61f74b619f06feb2ed6b12f42476e862c
Kostadin Bundov <Kostadinbundov@gmail.com>
2024-04-15 22:56:57 +0300

    Demo

---
+++ Week.05/01.Technician_And_Their_Work/Category.h
@@ -0,0 +1,12 @@
+#pragma once
+
+enum class ApplianceCategory {
+	Unknown = 1,
+	Phone = 2,
+	Laptop = 4,
+	TV = 8,
+	WashingMachine = 16,
+	Spotlight = 32,
+	Camera = 64,
+	AirConditioning = 128
+};
